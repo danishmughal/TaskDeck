c26acdaad4ed94206595044b20b30f20ed4900b7
Danish Mughal <mughal.danish@gmail.com>
2012-06-16 12:12:15 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskdeck::Application.config.secret_token = 'fd3026c6efef3415285d37cbee35b0aa0aa18bc83eca782716232734bfe98d72b9614cdd020b5cc0a97fdab85819d13fa9d994517c6a8dcca421d2f5d6380f01'
