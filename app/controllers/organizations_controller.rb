aa329f7d8854783ee7656956ea7bd221c18884f9
Danish Mughal <mughal.danish@gmail.com>
2012-06-20 16:49:22 +0500

    Added some basic security

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,28 +5,39 @@ class OrganizationsController < ApplicationController
 	end
 
 	def create
-		@organization = Organization.new(params[:organization])
-		if @organization.save
-			current_user.update_attribute(:organization_id, @organization.id)
-			flash[:success] = "Organization successfully created"
-			redirect_to '/'
+		if current_user.organization_id.nil?
+			@organization = Organization.new(params[:organization])
+			if @organization.save
+				current_user.update_attribute(:organization_id, @organization.id)
+				sign_in current_user
+				flash[:success] = "Organization successfully created"
+				redirect_to '/'
+			else
+				render 'new'
+			end
 		else
-			render 'new'
-		end
+			flash[:error] = "Invalid Request"
+	  		redirect_to '/'
+	  	end
 	end
 
 
 	def joinorg
-		username = params[:organization][:username]
-		password = params[:organization][:password]
-  		@orgs = Organization.find_by_username_and_password username, password
-  		if @orgs
-  			@joinedtrue = true
-  			current_user.update_attribute(:organization_id, @orgs.id)
-   			flash.now[:success] = @orgs.full_name
-  		else
-  			flash[:error] = "Incorrect username/password combination"
-  		end
+		if current_user.organization_id.nil?
+			username = params[:organization][:username]
+			password = params[:organization][:password]
+	  		@orgs = Organization.find_by_username_and_password username, password
+	  		if @orgs
+	  			@joinedtrue = true
+	  			current_user.update_attribute(:organization_id, @orgs.id)
+	  			sign_in current_user
+	  		else
+	  			flash[:error] = "Incorrect username/password combination"
+	  		end
+	  	else
+	  		flash[:error] = "Invalid Request"
+	  		redirect_to '/'
+	  	end
 	end
 
 end
