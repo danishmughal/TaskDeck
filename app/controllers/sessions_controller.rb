cb7b3b683c444f60abbe4ae50bb4cdacbf395d21
Danish Mughal <mughal.danish@gmail.com>
2012-07-06 12:29:05 +0500

    User profiles, initial teampanel complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to root_path
-			flash[:success] = "Welcome back to TaskDeck. You have X notifications."
+			flash[:success] = "Welcome to TaskDeck."
 		else
 			flash.now[:error] = 'Invalid email/password combination. Please try again.'
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,4 +55,8 @@ class UsersController < ApplicationController
 	end
 
 
+	def show
+		@user = User.find(params[:id])		
+	end
+
 end
