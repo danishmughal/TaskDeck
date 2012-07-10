f563d3f028d6fb5d479fcfbceb18d8409519cc7e
Danish Mughal <mughal.danish@gmail.com>
2012-07-10 13:09:04 +0500

    mailer work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap
+
+
+
+
+​
+​
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "gmail.com",  
-  :user_name            => "TaskDeckMailer",  
+  :user_name            => "taskdeckmailer",  
   :password             => "Demo1234",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
-}  
+}
